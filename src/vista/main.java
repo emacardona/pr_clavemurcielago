32720d0ba46642f3c45b1ce5a94d1695beb712af
emacardona <ediaz3157@gmail.com>
2024-08-16 19:51:55 -0600

    tarea clave muercielago

---
+++ src/vista/main.java
@@ -0,0 +1,12 @@
+
+package vista;
+
+public class main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_murcielago frm = new frm_murcielago();
+        frm.show();
+    }
+    
+}
